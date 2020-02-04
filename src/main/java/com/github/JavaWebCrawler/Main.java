5c1d583ef2312db47c0fe1e91a9832c6f8e661bd
Jason <840495823@qq.com>
2020-02-04 19:21:27 +0800

    changedDataStructureOfDB

--- src/main/java/com/github/JavaWebCrawler/Main.java
+++ src/main/java/com/github/JavaWebCrawler/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         CrawlerDao dao = new MyBatisCrawlerDao();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 4; i++) {
             new Crawler(dao).start();
         }
-
     }
 }
