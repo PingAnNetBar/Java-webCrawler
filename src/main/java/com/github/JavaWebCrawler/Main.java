ffea2d8435078bad38c70430ec6e14577cba2f88
Jason <840495823@qq.com>
2020-02-07 19:28:02 +0800

    do it

--- src/main/java/com/github/JavaWebCrawler/Main.java
+++ src/main/java/com/github/JavaWebCrawler/Main.java
@@ -2,6 +2,8 @@ package com.github.JavaWebCrawler;
 
 public class Main {
 
+    //let me do this
+
     public static void main(String[] args) {
         CrawlerDao dao = new MyBatisCrawlerDao();
         for (int i = 0; i < 4; i++) {
