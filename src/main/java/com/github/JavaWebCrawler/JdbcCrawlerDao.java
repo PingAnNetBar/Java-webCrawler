6c21026b6eb911984eb2fc18aa5ae56d2b19456f
Jason <840495823@qq.com>
2020-02-07 18:46:48 +0800

    Demo02

--- src/main/java/com/github/JavaWebCrawler/JdbcCrawlerDao.java
+++ src/main/java/com/github/JavaWebCrawler/JdbcCrawlerDao.java
@@ -19,7 +19,7 @@ public class JdbcCrawlerDao implements CrawlerDao {
         }
     }
 
-    //getNextLink方法并不是所有实现都要需要遵守的协议，流程中并没有用到它，只是辅助getNextLinkThenDelete方法，故设置为JdbcCrawlerDao的private方法
+    //getNextLink方法并不是所有实现都要需要遵守的协议，流程中并没有用到它，只是辅助本类下getNextLinkThenDelete方法，故设置为JdbcCrawlerDao的private方法
     private String getNextLink(String sql) throws SQLException {
         ResultSet resultSet = null;
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -34,8 +34,9 @@ public class JdbcCrawlerDao implements CrawlerDao {
         }
         return null;
     }
+
     @Override
-    public String getNextLinkThenDelete() throws SQLException {
+    public synchronized String getNextLinkThenDelete() throws SQLException {
         String link = getNextLink("select link from LINKS_TO_BE_PROCESSED limit 1");
         if (link != null) {
             updateDatabase(link, "delete from LINKS_TO_BE_PROCESSED where link = ?");
@@ -49,39 +50,56 @@ public class JdbcCrawlerDao implements CrawlerDao {
             statement.executeUpdate();
         }
     }
+
     @Override
     public void insertNewsIntoDataBase(String title, String url, String content) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement("insert into news (title,url,content,created_at,modified_at) values(?,?,?,now(),now())")) {
+        try (PreparedStatement statement = connection.prepareStatement("insert into news (title,url,content,created_at) values(?,?,?,now())")) {
             statement.setString(1, title);
             statement.setString(2, url);
             statement.setString(3, content);
             statement.executeUpdate();
         }
     }
+
     @Override
-    public boolean isProcessed(String link) throws SQLException {
-        ResultSet resultSet = null;
-        try (PreparedStatement statement = connection.prepareStatement("select link from LINKS_ALREADY_PROCESSED where link = ?")) {
+    public void insertLinkAlreadyProcessed(String link) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("insert into LINKS_ALREADY_PROCESSED(LINK) values (?)")) {
             statement.setString(1, link);
+            statement.executeUpdate();
+        }
+    }
+
+
+    @Override
+    public void insertLinkToBeProcessed(String href) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("insert into LINKS_TO_BE_PROCESSED(LINK) values (?)")) {
+            statement.setString(1, href);
+            statement.executeUpdate();
+        }
+    }
+
+    @Override
+    @SuppressFBWarnings("OBL_UNSATISFIED_OBLIGATION")
+    public boolean isDistincted(String title) throws SQLException {
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.prepareStatement("select title from NEWS where title = ?");
+            statement.setString(1, title);
             resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 return true;
             }
-        } finally {
-            if (resultSet != null) {
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }finally {
+            if (statement!=null){
+                statement.close();
+            }
+            if (resultSet!=null){
                 resultSet.close();
             }
         }
         return false;
-    }
-
-    @Override
-    public void insertLinkAlreadyProcessed(String link) {
-
-    }
-
-    @Override
-    public void insertLinkToBeProcessed(String href) {
-
     }
 }
--- src/main/resources/db/migration/V1__Initial_tables.sql
+++ src/main/resources/db/migration/V1__Initial_tables.sql
@@ -6,8 +6,8 @@ url varchar(1000),
 created_at timestamp default now()
 ) DEFAULT CHARSET=utf8mb4;
 
-create table LINKS_TO_BE_PROCESSED (link varchar(1000));
-create table LINKS_ALREADY_PROCESSED (link varchar(1000));
+create table `LINKS_TO_BE_PROCESSED` (link varchar(1000));
+create table `LINKS_ALREADY_PROCESSED` (link varchar(1000));
 
 --alter table links_to_be_processed convert to character set utf8;
 --alter table links_already_processed convert to character set utf8;
